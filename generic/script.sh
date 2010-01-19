6465067ba8ea4b02d9d772bfe9d08b942a83e68b
Gabriel <g2p.code@gmail.com>
2010-01-19 23:49:51 +0100

    PostgreSQL Makefile.

--- generic/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for H in $(< planets.hosts.list.broken); do
-    wget --continue --timestamping --force-directories --directory-prefix cache http://"$H"/opml.xml
-    xmlstarlet tr opml.xsl -s planetHostname="$H" cache/"$H"/opml.xml > cache/"$H"/opml+tags.xml
-done
